5eec3768b62ca3f808be2bf7a7a8b0bdd63dbc40
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-04-27 17:52:45 -0400

    made changes

--- test/main.test.js
+++ test/main.test.js
@@ -1,7 +1,16 @@
+const expect = require("chai").expect
+
 const averageScore = require("../src/main.js");
 
 describe('averageScore', () => {
     it("should output avg score", () => {
-        
+        const input = [
+            { name: "Shane Carey", score: 9.5 },
+            { name: "Rebecca Mills", score: 8.7 },
+            { name: "Catarina Lima", score: 9.7 },
+          ];
+          const expected = 9.7;
+          const actual = averageScore(input);
+          expect(actual).to.equal(expected)
     })
 })
